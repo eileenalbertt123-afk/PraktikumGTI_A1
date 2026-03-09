7131c4d4c9b54062f6c849f2b57f3c659a41d347
eileenalbertt123-afk <eileenalbertt123@gmail.com>
2026-03-10 00:59:16 +0700

    Upload source code no 2

--- Tugas1/main.cpp
+++ Tugas1/main.cpp
@@ -3,34 +3,86 @@
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3f(0,0,0); 
-
-    glPointSize(10);
-    glBegin(GL_POINTS);
-    glVertex2f(-0.8, 0.8);
+    // GL_LINE_STRIP
+    glColor3f(0.8, 0.3, 0.3); 
+    glBegin(GL_LINE_STRIP);
+        glVertex2f(-0.9, 0.75);
+        glVertex2f(-0.7, 0.55);
+        glVertex2f(-0.5, 0.75);
+        glVertex2f(-0.3, 0.55);
     glEnd();
 
-    glBegin(GL_LINES);
-    glVertex2f(-0.6, 0.8);
-    glVertex2f(-0.4, 0.6);
+    // GL_LINE_LOOP
+    glColor3f(0.3, 0.7, 0.3);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(0.0, 0.75);
+        glVertex2f(0.2, 0.55);
+        glVertex2f(0.4, 0.75);
+        glVertex2f(0.2, 0.95);
+    glEnd();
+
+    // GL_TRIANGLE_FAN
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(-0.7, 0.15);
+    glVertex2f(-0.9, 0.0);
+    glVertex2f(-0.8, -0.2);
+    glVertex2f(-0.6, -0.2);
+    glVertex2f(-0.5, 0.0);
+	glEnd();
+
+    // GL_TRIANGLE_STRIP
+    glColor3f(0.8, 0.8, 0.3);
+    glBegin(GL_TRIANGLE_STRIP);
+        glVertex2f(-0.2, 0.1);
+        glVertex2f(-0.2, -0.35);
+        glVertex2f(0.1, 0.1);
+        glVertex2f(0.1, -0.35);
+        glVertex2f(0.4, 0.1);
+        glVertex2f(0.4, -0.35);
+    glEnd();
+
+    // GL_QUADS
+    glColor3f(0.8, 0.4, 0.7);
+    glBegin(GL_QUADS);
+        glVertex2f(0.5, 0.1);
+        glVertex2f(0.7, 0.1);
+        glVertex2f(0.7, -0.35);
+        glVertex2f(0.5, -0.35);
+    glEnd();
+
+    // GL_QUAD_STRIP
+    glColor3f(0.4, 0.8, 0.8);
+    glBegin(GL_QUAD_STRIP);
+        glVertex2f(0.75, 0.1);
+        glVertex2f(0.75, -0.35);
+
+        glVertex2f(0.9, 0.1);
+        glVertex2f(0.9, -0.35);
+
+        glVertex2f(1.05, 0.1);
+        glVertex2f(1.05, -0.35);
     glEnd();
 
     glFlush();
 }
 
+void init() {
+    glClearColor(0.95, 0.95, 0.95, 1.0); // background abu muda
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluOrtho2D(-1.2, 1.2, -1.2, 1.2); // memperluas area tampilan
+}
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(500,500);
-    glutCreateWindow("Tugas no 1");
+    glutInitWindowSize(900,600);
+    glutCreateWindow("Primitive OpenGL");
 
-    glClearColor(1,1,1,1);
-
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(-1,1,-1,1);
-
+    init();
     glutDisplayFunc(display);
-    glutMainLoop();
 
-    return 0;
+    glutMainLoop();
 }
